fe3a344d2e59a9f04017094061b950181acedfa0
Mudasir <mudasiramin217@gmail.com>
2026-05-03 10:16:34 +0530

    new soln

---
+++ Codeforces/800/1325A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        cout<<1 <<" "<<x-1<<endl;
+
+    }
+    return 0;
+}
+//here we are using gcd +lcm=suppose its x so here x=g+g*m*n ==g(1+mn) take g=1 and mn is x-1 with 
+//solved proof;
