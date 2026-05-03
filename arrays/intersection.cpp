26e1b587cc23e2fe57195a636e1c0a436fceafe8
Mudasir <mudasiramin217@gmail.com>
2026-05-03 17:59:44 +0530

    new app

--- arrays/intersection.cpp
+++ arrays/intersection.cpp
@@ -24,3 +24,32 @@ class Solution {
         return ans;//BRUTE FORCE GOT TLE; BUT VALID ANS.
     }
 };
+//optimal approach;
+class Solution {
+  public:
+    vector<int> intersection(vector<int> &arr1, vector<int> &arr2) {
+        int n =arr1.size();
+        int m =arr2.size();
+        int i=0;
+        int j=0;
+        vector<int>ans;
+        while(i<n &&j<m){
+            if(arr1[i]<arr2[j]){
+                i++;
+            }
+            else if(arr1[i]>arr2[j]){
+                j++;
+            }
+            else{
+                if(ans.size()==0 ||ans.back()!=arr1[i]){
+                    ans.push_back(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        
+        
+        return ans; 
+    }
+};
