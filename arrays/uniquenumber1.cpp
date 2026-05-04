8726a50e78f520ccf0a2caf30b28a328dc05d1d7
Mudasir <mudasiramin217@gmail.com>
2026-05-04 22:15:19 +0530

    new app

--- arrays/uniquenumber1.cpp
+++ arrays/uniquenumber1.cpp
@@ -20,3 +20,28 @@ class Solution {
     }
 };
 //better:
+ class Solution {
+  public:
+    int findUnique(vector<int> &arr) {
+        int n = arr.size();
+
+        int maxi = arr[0];
+        for(int i = 0; i < n; i++){
+            maxi = max(maxi, arr[i]);
+        }
+
+        vector<int> hash(maxi + 1, 0); 
+
+        for(int i = 0; i < n; i++){
+            hash[arr[i]]++;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(hash[arr[i]] == 1){
+                return arr[i];
+            }
+        }
+
+        return -1;   
+    }
+};
