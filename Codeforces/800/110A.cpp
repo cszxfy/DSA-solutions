5ad85954d1cb3c2ab942a9d355774c7ca1b68027
Mudasir <mudasiramin217@gmail.com>
2026-05-06 20:05:30 +0530

    new soln

---
+++ Codeforces/800/110A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long n;
+    cin >> n;
+    int count = 0;
+    while (n > 0) {
+        int digit = n % 10;
+
+        if (digit == 4 || digit == 7)
+            count++;
+
+        n /= 10;
+    }
+    bool lucky = true;
+    if (count == 0)
+        lucky = false;
+    while (count > 0) {
+        int digit = count % 10;
+        if (digit != 4 && digit != 7) {
+            lucky = false;
+            break;
+        }
+        count /= 10;
+    }
+    if (lucky)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
