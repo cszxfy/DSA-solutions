d5a3f7069a663187fe5c65ed9d86396d07defd73
Mudasir <mudasiramin217@gmail.com>
2026-05-10 23:14:34 +0530

    new app

--- arrays/towsum.cpp
+++ arrays/towsum.cpp
@@ -31,3 +31,33 @@ public:
         return{};
     }
 };
+//slightly better appraoch(but here space comp inc):
+ class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int,int>> arr;
+        for(int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i});
+        }
+        sort(arr.begin(), arr.end());
+        int left = 0;
+        int right = n - 1;
+        while(left < right) {
+            int sum = arr[left].first + arr[right].first;
+            if(sum == target) {
+                return {
+                    arr[left].second,
+                    arr[right].second
+                };
+            }
+            else if(sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return {};
+    }
+};
