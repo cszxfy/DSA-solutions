7d1d675a91aec97dc53ecf44554374128d3b6739
Mudasir <mudasiramin217@gmail.com>
2026-05-12 20:42:08 +0530

    new soln

---
+++ arrays/majorityelement1.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+//brute soln:
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n =nums.size();
+        for(int i=0;i<n;i++){
+            int cnt =0;
+            for(int j=0;j<n;j++){
+                if(nums[i]==nums[j])
+                   cnt++;
+            }
+            if(cnt>n/2){
+            return nums[i];
+        }
+        }
+        return -1;
+    }
+};
+//better using hasing:
+
+//optimal soln(Moore's voting algo ):
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n =nums.size();
+        int cnt=0,ele=0;
+        for(int i =0;i<n;i++){
+            if(cnt==0){
+                cnt=1;
+                ele=nums[i];
+            }
+            else if(nums[i]==ele){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        int cnt1=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele) cnt1++;
+        }
+        if(cnt1>n/2){
+            return ele;
+        }
+        return -1;
+    }
+};
+
