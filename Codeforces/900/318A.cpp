cf262315ee427a57ebb298c1bf0baae0f4297dfa
Mudasir <mudasiramin217@gmail.com>
2026-05-23 17:20:46 +0530

    new soln

---
+++ Codeforces/900/318A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long n, k;
+    cin >> n >> k;
+    long long odd = (n + 1) / 2;
+    if(k <= odd) {
+        cout << 2 * k - 1;
+    }
+    else {
+        k = k - odd;
+        cout << 2 * k;
+    }
+    return 0;
+}
