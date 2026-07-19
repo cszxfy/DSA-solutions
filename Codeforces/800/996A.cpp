2386c86fc7d42e17f48b66ca82d3d3102bba178d
Mudasir <mudasiramin217@gmail.com>
2026-07-19 16:39:31 +0530

    new soln

---
+++ Codeforces/800/996A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[] = {100, 20, 10, 5, 1};
+    int ans = 0;
+    for (int i = 0; i < 5; i++) {
+        ans += n / arr[i];
+        n = n % arr[i];
+    }
+    cout << ans;
+    return 0;
+}
