15a14fc872758736d360ecd991c1f9fa182cb2a3
Mudasir <mudasiramin217@gmail.com>
2026-07-22 00:15:44 +0530

    new soln

---
+++ arrays/binaryserach.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n =nums.size();
+        int high=n-1;
+        int low=0;
+        while(low<=high){
+            int mid=(high+low)/2;
+            if(nums[mid]==target) return mid;
+            else if(target>nums[mid]) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
+    }
+};
