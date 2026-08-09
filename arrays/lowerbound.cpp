98a6622e6cdb382110dfc0abee17129eaf5785f9
Mudasir <mudasiramin217@gmail.com>
2026-08-09 20:41:49 +0530

    new question

---
+++ arrays/lowerbound.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+     int n=arr.size();
+     int low=0,high=n-1;
+     while(low<=high){
+         int mid=low+(high-low)/2;
+         if(arr[mid]>=target){
+             n=mid;
+             high=mid-1;
+         }
+         else{
+             low=mid+1;
+         }
+     }
+     return n ;
+     }
+};
