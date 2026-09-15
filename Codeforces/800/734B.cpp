52ba1cb393199df6c2ec3c1e68b969f4ff087908
Mudasir <mudasiramin217@gmail.com>
2026-09-15 13:18:27 +0530

    nw soln

--- Codeforces/800/734B.cpp
+++ Codeforces/800/734B.cpp
@@ -4,12 +4,18 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    for(int i =0;i<4;i++){
-        int x ;
-        cin>>x;
-        
-    }
-
+    long long k2, k3, k5, k6;
+    cin >> k2 >> k3 >> k5 >> k6;
+    long long arr[4] = {k2, k3, k5, k6};
+    long long x = min(arr[0], arr[1]);
+    arr[0] -= x;
+    arr[1] -= x;
+    long long y = min(arr[0], min(arr[2], arr[3]));
+    arr[0] -= y;
+    arr[2] -= y;
+    arr[3] -= y;
+    long long ans = x * 32 + y * 256;
+    cout << ans << endl;
+    
     return 0;
 }
